0f31586f62e74a2bfcb809587a072d60fee02e7e
lbay <lbay@10.120.9.142>
2019-11-13 10:59:14 -0500

    fgkfkcfjdfkfg;hifib;h

--- Bay, Luc/DSStuff/calculator/Calculator.java
+++ Bay, Luc/DSStuff/calculator/Calculator.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import java.util.Stack;
+import java.lang.Math;
 
 public class Calculator 
 {
@@ -17,7 +18,13 @@ public class Calculator
 			if(Character.isDigit(temp))
 				nums.push((double) Character.getNumericValue(temp));
 			else if(temp != ' ')
-				ops.push(temp);
+			{
+				if(checksign(temp))
+					ops.push(temp);
+				else
+					operate();
+			}
+				
 		}
 		
 	}
@@ -33,10 +40,53 @@ public class Calculator
 		int x = 0;
 	}
 	
-	public static boolean checksign(char s1, char s2)
+	public static boolean checksign(char s1)
 	{
+		char s2 = ops.lastElement();
+		int n1 = 0, n2 = 0;
 		
-		return false;
+		switch (s1)
+		{
+			case '+' : case '-' : n1 = 1;
+				break;
+			case '/' : case '*' : n1 = 2;
+				break;
+			case '^' : n1 = 3; 
+				break;
+		}
+		
+		switch (s2)
+		{
+			case '+' : case '-' : n2 = 1;
+				break;
+			case '/' : case '*' : n2 = 2;
+				break;
+			case '^' : n2 = 3;
+				break;
+		}
+		
+		return n1 > n2;
+	}
+	
+	public static void operate()
+	{
+		while(checksign())
+		{
+		Double n1 = nums.pop();
+		Double n2 = nums.pop();
+		Double n3 = 0.0;
+		
+		switch (ops.pop())
+		{
+		case '+' : n3 = n1+n2; break;
+		case '-' : n3 = n1-n2; break;
+		case '*' : n3 = n1*n2; break;
+		case '/' : n3 = n1/n2; break;
+		case '^' : n3 = Math.pow(n1, n2); break;
+		}
+		
+		nums.push(n3);
+		}
 	}
 
 }
