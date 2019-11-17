8f4bda8cb1f1af7f75c1f9dd49409afa778d1806
lucba <lucba@LucLaptop>
2019-11-16 20:36:25 -0500

    works

--- Bay, Luc/DSStuff/calculator/Calculator.java
+++ Bay, Luc/DSStuff/calculator/Calculator.java
@@ -5,109 +5,111 @@ import java.lang.Math;
 
 public class Calculator 
 {
-	static Stack<Double> nums;
-	static Stack<Character> ops;
-	static String exp;
+	static Stack<Double> nums = new Stack<Double>();
+	static Stack<String> ops = new Stack<String>();
+	static String exp = "2 * ( 13 - ( 1 + 6 ) )";		// Enter expression here-----------
 	
-	public static void enter()
+	public static void main(String[] args) throws Exception 
 	{
+		String elem = "";
+		
 		for(int i=0; i<exp.length(); i++)
 		{
-			char temp = exp.charAt(i);
-
-			if(Character.isDigit(temp))
-				nums.push((double) Character.getNumericValue(temp));
-			else if(temp != ' ')
+			if(exp.charAt(i) == ' ') 							// element is complete
 			{
-				if(checksign(temp))
-					ops.push(temp);
-				else
-					while(!ops.isEmpty())
-						operate();
+				parse(elem);
+				elem = "";
 			}
-				
+			else												// element is not complete, continue adding
+				elem += exp.charAt(i);
+		}
+		
+		parse(elem);
+		while(!ops.isEmpty())									// take care of remaining expression
+			operate();
+		
+		System.out.println("Expression: "+exp);
+		System.out.println("Answer: "+nums.pop());
+	}
+	
+	public static void parse(String elem) throws Exception
+	{
+		if(isNumber(elem)) 										// element is #, push to stack
+			nums.push(Double.parseDouble(elem));
+		else													// element is a sign
+		{
+			if(elem.equals(")"))								// element is a closed parenthesis
+			{
+					while(!ops.lastElement().equals("("))		// operate until open parenthesis found on stack
+						operate();
+					ops.pop();
+			}
+			else if(checkprec(elem))							// element has higher prec, push to stack
+				ops.push(elem);
+			else												// element has lower prec
+			{
+				while(!checkprec(elem))							// operate until lower prec then push to stack
+					operate();
+				ops.push(elem);
+			}
 		}
 	}
 	
-	
-	public static void main(String[] args) 
+	public static boolean isNumber(String elem)					// Returns if the given string contains only numbers
 	{
-		exp = "1 + 2 + 3";
-		nums = new Stack<Double>();
-		ops = new Stack<Character>();
-		
-		enter();
-		int x = 0;
+	    try{double x = Double.parseDouble(elem);}
+	    		
+	    catch (NumberFormatException | NullPointerException nfe){return false;}
+
+	    return true;
 	}
 	
-	public static boolean checksign(char s1)
+	public static boolean checkprec(String s1)					// Returns whether given sign has prec over the sign at the top of the stack
 	{
-		if(ops.empty())
+		if(ops.empty())											// Automatically has prec if 1st sign
 			return true;
 		
-		char s2 = ops.lastElement();
+		String s2 = ops.lastElement();
 		int n1 = 0, n2 = 0;
 		
 		switch (s1)
 		{
-			case '+' : case '-' : n1 = 1;
-				break;
-			case '/' : case '*' : n1 = 2;
-				break;
-			case '^' : n1 = 3; 
-				break;
+			case "+" : case "-" : n1 = 1; break;
+			case "/" : case "*" : n1 = 2; break;
+			case "^" : n1 = 3; break;
+			case ")" : case "(" : n1 = 4; break;
 		}
 		
 		switch (s2)
 		{
-			case '+' : case '-' : n2 = 1;
-				break;
-			case '/' : case '*' : n2 = 2;
-				break;
-			case '^' : n2 = 3;
-				break;
+			case "(" : n2 = 0; break;
+			case "+" : case "-" : n2 = 1; break;
+			case "/" : case "*" : n2 = 2; break;
+			case "^" : n2 = 3; break;
+			case ")" : n2 = 4; break;
 		}
 		
-		return n1 >= n2;
+		return n1 > n2;
 	}
 	
-	public static void operate()
+	public static void operate() throws Exception				// Does the actual math
 	{
-		char op = ops.pop();
-		Double n1 = nums.pop();
-		Double n2 = nums.pop();
+		String op = ops.pop();
+		Double n1 = nums.pop(), n2 = nums.pop();
+		
+		if(op.equals("/") && n1 == 0)
+			throw new Exception("You can't / by 0 IDIOT");
 	
 		switch (op)
 		{
-		case '+' : n1 += n2; break;
-		case '-' : n1 -= n2; break;
-		case '*' : n1 *= n2; break;
-		case '/' : n1 /= n2; break;
-		case '^' : n1 = Math.pow(n1, n2); break;
+		case "+" : n2 += n1; break;
+		case "-" : n2 -= n1; break;
+		case "*" : n2 *= n1; break;
+		case "/" : n2 /= n1; break;
+		case "^" : n2 = Math.pow(n2, n1); break;
 		}
 	
-		nums.push(n1);
-		
-		/*char op = ops.pop();
-		
-		while(checksign(op) && !ops.isEmpty())
-		{
-			Double n1 = nums.pop();
-			Double n2 = nums.pop();
-		
-			switch (op)
-			{
-			case '+' : n1 += n2; break;
-			case '-' : n1 -= n2; break;
-			case '*' : n1 *= n2; break;
-			case '/' : n1 /= n2; break;
-			case '^' : n1 = Math.pow(n1, n2); break;
-			}
-		
-			nums.push(n1);
-			//if(!ops.isEmpty())
-				op = ops.pop();
-		}*/
+		nums.push(n2);
 	}
 
 }
